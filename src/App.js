3a8cfab8fb8deeb887607ae6515b401969a1293a
Joe Dukes <joedukes.email@gmail.com>
2019-07-10 16:36:26 -0400

    updates to App.js, form.js, results.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { map } from 'ramda';
-// import logo from './logo.svg';
+import Form from './form';
+import Results from './results';
 import './App.css';
 
 const token =
@@ -11,74 +11,38 @@ function searchMovies(criteria) {
     headers: {
       authorization: 'Bearer ' + token
     }
-  }).then(response => response.json);
+  }).then(response => response.json());
 }
 
 function App() {
-  const [criteria, setCriteria] = useState('');
   const [results, setResults] = useState([]);
-  const [fromState, setValidState] = useState({ empty: true, valid: true });
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    // console.log('find movies');
-    // // call an api with criteria to get a list of movies
+  function handleSearch(criteria) {
     searchMovies(criteria)
       .then(v => {
         console.log(v);
+        return v;
       })
       .then(searchResults => setResults(searchResults.Search));
   }
 
-  function handleChange(e) {
-    setCriteria(e.target.value);
-
-    function validate() {
-      if (criteria.length > 0) {
-        if (criteria.toLowerCase() === 'cloverfield') {
-          setValidState;
-        }
-      }
-    }
-  }
   return (
     <section className='section'>
       <div className='container'>
         <div className='columns'>
           <div className='column'>
             <h1 className='title'>Movie Search</h1>
-            <form className='form' onSubmit={handleSubmit}>
-              <div className='field'>
-                <label className='label'>Name:</label>
-                <div className='control'>
-                  <input
-                    type='text'
-                    className='input'
-                    value={criteria}
-                    onChange={handleChange}
-                    onBlur={validate}
-                  />
-                </div>
-                <div className='help'>Please enter a name of a movie</div>
-              </div>
-              <div className='field'>
-                <button className='button is-primary' type='submit'>
-                  Search
-                </button>
-              </div>
-            </form>
+            <Form onSearch={handleSearch} />
+          </div>
+          <div className='column'>
             <div className='box'>
-              <ul>{map(li, results)}</ul>
+              <Results movies={results} />
             </div>
           </div>
         </div>
       </div>
     </section>
   );
-}
-
-function li(movie) {
-  return <li key={movie.imdbID}>{movie.Title}</li>;
 }
 
 export default App;
